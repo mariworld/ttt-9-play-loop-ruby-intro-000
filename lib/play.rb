876995b3322cc89cd6e1e6e4184d99d99d3d2464
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-20 16:23:07 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,6 +38,15 @@ end
 # Define your play method below
 def play(board)
     turn(board)
+    turn(board)
+    turn(board)
+    turn(board)
+    turn(board)
+    turn(board)
+    turn(board)
+    turn(board)
+    turn(board)
+    turn(board)
     while turn < 9
         turn +=1
     end
